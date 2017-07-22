24fb179e671b910aab290d15c2ab274ffa48da67
Axel Feldmann <axel.s.feldmann@gmail.com>
2017-07-22 13:58:46 -0700

    wired in a database

--- server/lib/index.js
+++ server/lib/index.js
@@ -2,11 +2,28 @@ import express from 'express';
 
 var app = express();
 const port = 3001;
- 
+
+import { MongoClient } from 'mongodb';
+
+//todo: hide my password and stuff somewhere
+const dburl =
+'mongodb://axel:king@ds045785.mlab.com:45785/predictions-app';
+
+var db;
+
+//routes
+
 app.get('/api', (req, res) => {
   res.json({ text: 'Hello World' });
 });
- 
-app.listen(port);
 
-console.log('server running on ' + port);
+//start up the server only if the database is running
+MongoClient.connect(dburl, (err, database) => {
+  if(err) return console.log(err);
+  db = database;
+  app.listen(port, () => {
+    console.log('server running on ' + port);
+  });
+});
+ 
+
