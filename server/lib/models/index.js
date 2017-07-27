1cba895f66c794e2dc61ea78271f0922d484c1af
Axel Feldmann <axel.s.feldmann@gmail.com>
2017-07-26 23:04:01 -0700

    removed some extraneous commented code

--- server/lib/models/index.js
+++ server/lib/models/index.js
@@ -16,22 +16,3 @@ export const connect = (uri) => {
   require('./user');
   require('./league');
 }
-
-
-// var mongoose = require('mongoose'),
-//     Schema = mongoose.Schema,
-//     autoIncrement = require('mongoose-auto-increment');
- 
-// var connection = mongoose.createConnection("mongodb://localhost/myDatabase");
- 
-// autoIncrement.initialize(connection);
- 
-// var bookSchema = new Schema({
-//     author: { type: Schema.Types.ObjectId, ref: 'Author' },
-//     title: String,
-//     genre: String,
-//     publishDate: Date
-// });
- 
-// bookSchema.plugin(autoIncrement.plugin, 'Book');
-// var Book = connection.model('Book', bookSchema);
