0d3ce46f694f70f00fb17f3404524b613d5abf7f
Axel Feldmann <axel.s.feldmann@gmail.com>
2017-07-29 20:59:43 -0700

    starting league route stuff

--- client/src/components/main-content/leagues.js
+++ client/src/components/main-content/leagues.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Actions from '../../actions/actions';
+import LeagueView from './league-view';
+import { Route } from 'react-router';
 
 const mapStateToProps = (state) => ({
   loading: state.leagues.loading,
   error: state.leagues.error,
   leagues: state.leagues.leagues,
-  token: state.auth.token
+  token: state.auth.token,
+  me: state.auth.username
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -21,6 +24,16 @@ class Leagues extends React.Component{
 
   }
 
+  generateRoutes(leagues){
+    return leagues.map(({ _id, name, creator }) => {
+      return (<Route path={ _id.toString() } key={ _id } render={ () => <LeagueView name={ name }/> }/>);
+    });
+  }
+
+  listLeagues(leagues){
+    return (<div>{ JSON.stringify(leagues) }</div>);
+  }
+
   render(){
     const { loading, error, leagues } = this.props;
 
@@ -30,7 +43,12 @@ class Leagues extends React.Component{
     if(error){
       return (<h1>{ error }</h1>);
     }
-    return (<div>{ JSON.stringify(leagues) }</div>);
+    return (
+      <div>
+        { this.generateRoutes(leagues) }
+        { this.listLeagues(leagues) }
+      </div>
+    );
   }
 }
 
