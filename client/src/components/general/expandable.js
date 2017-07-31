7d6c19f5e9cd2fcbbe3a6bd8e33e5386b84dad7e
Axel Feldmann <axel.s.feldmann@gmail.com>
2017-07-30 18:34:41 -0700

    expandable widgets on league view page put in

---
+++ client/src/components/general/expandable.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import classnames from 'classnames';
+
+const Expandable = ({ toggleFn, expanded, children, label }) => {
+  const labelClasses = classnames('expandable-label', {
+    'expanded': expanded,
+    'collapsed': !expanded
+  });
+
+  const chevronClasses = classnames('chevron', {
+    'bottom': expanded,
+    'right': !expanded
+  });
+
+  return (
+    <div  className='expandable-container' onClick={ toggleFn }>
+      <div className={ labelClasses }>
+        <span className={ chevronClasses }/>
+        <label>{ label }</label>
+      </div>
+      { (expanded) ? <div className='expanded-content'>{ children }</div> : null }
+    </div>
+  );
+};
+
+export default Expandable;
--- client/src/components/main-content/league-view.js
+++ client/src/components/main-content/league-view.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Actions from '../../actions/actions';
+import Expandable from '../general/expandable';
 
 const mapStateToProps = (state) => ({
   token: state.auth.token,
   league: state.league.league,
   error: state.league.error,
-  loading: state.league.loading
+  loading: state.league.loading,
+  username: state.auth.username
 });
 
 const mapDispatchToProps = (dispatch) => ({
@@ -15,18 +17,60 @@ const mapDispatchToProps = (dispatch) => ({
 
 class LeagueView extends React.Component{
 
+  constructor(){
+    super();
+    this.state = { membersExp: false, inviteExp: false, contentExp: false };
+    this.toggleFn = this.toggleFn.bind(this);
+  }
+
   componentDidMount(){
-    console.log(this.props);
     this.props.getLeague(this.props.token, this.props.match.params.leagueID);
   }
 
+  toggleFn(field){
+    return () => {
+      const update = {};
+      update[field] = !this.state[field];
+      this.setState(update);
+    };
+  }
+
   render(){
-    const { loading, error, league} = this.props;
+    const { loading, error, league, username } = this.props;
+    const isCreator = ( league.creator === username);
     if(loading)
       return (<h1>loading...</h1>);
     if(error)
       return (<h1>{ error }</h1>);
-    return (<div>{ JSON.stringify(league)}</div>);
+    return (
+      <div className='league-view-container'>
+        { isCreator &&
+          /* only display invites widget if the person is the league creator */
+          <Expandable
+            toggleFn={ this.toggleFn('inviteExp') }
+            expanded={ this.state.inviteExp }
+            label='League Invites'
+          >
+            <h1>invites</h1>
+          </Expandable>
+        }
+        <Expandable
+          toggleFn={ this.toggleFn('membersExp') }
+          expanded={ this.state.membersExp }
+          label='League Members'
+        >
+          <h1>members</h1>
+        </Expandable>
+
+        <Expandable
+          toggleFn={ this.toggleFn('contentExp') }
+          expanded={ this.state.contentExp }
+          label='League Content'
+        >
+          <h1>content</h1>
+        </Expandable>
+      </div>
+    );
   }
 }
 
