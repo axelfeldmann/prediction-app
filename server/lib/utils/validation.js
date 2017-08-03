c031668d6ff9fa026b0a5d8e2d98ca0d86678bc3
Axel Feldmann <axel.s.feldmann@gmail.com>
2017-08-02 23:02:52 -0700

    changed flex to -webkit-flex for safari support

---
+++ server/lib/utils/validation.js
@@ -0,0 +1,11 @@
+export const validateUsername = (username) => {
+  const validationResult = { success: false, message: '' };
+};
+
+export const validatePassword = (password) => {
+  const validationResult = { success: false, message: '' };
+};
+
+export const validateLeagueName = (leagueName) => {
+  const validationResult = { success: false, message: '' };
+};
